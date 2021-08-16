a93caf5426a2be2c02a74bbae56c69af3122e8e5
cmack29 <conor.m@me.com>
2021-08-16 17:23:03 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/Header'
+import LeftSidebar from './components/LeftSidebar'
+import Main from './components/Main'
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <LeftSidebar />
+      <Main />
+    </div>
+  )
+}
+
+export default App
